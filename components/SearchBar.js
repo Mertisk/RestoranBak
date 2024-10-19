0a3f0bf9571774ab48ae60584c51d8492b43456d
mertisk <merrtt.isk@gmail.com>
2024-10-19 15:20:09 +0300

    search comp

---
+++ components/SearchBar.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+export default function SearchBar() {
+  return (
+    <View style={styles.backgroundStyle}>
+      <AntDesign
+        style={styles.iconStyle}
+        name="search1"
+        size={30}
+        color="black"
+      />
+      <TextInput
+        placeholder="Ara"
+        style={styles.inputStyle}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "lightgray",
+    flexDirection: "row",
+    margin: 10,
+    alignItems: "center",
+    height: 50,
+    borderRadius: 20,
+  },
+  iconStyle: {
+    marginHorizontal: 15,
+  },
+  inputStyle: {
+    flex: 1, //kalan yeri oldugu gibi kaplar
+    fontSize: 18,
+  },
+});
